a29325fdcfbe8c9c2e0e2f71747efd8f264184f9
Nischinth Murari <murarins92@gmail.com>
2025-11-27 02:41:25 -0500

    First rework for consuming mocks from ubk-commons

---
+++ contracts/mocks/UBKCommonsMocks.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Importing these ensures Hardhat generates artifacts for them. 
+import "@ubk-labs/ubk-commons/contracts/mocks/MockERC20.sol";
+import "@ubk-labs/ubk-commons/contracts/mocks/MockERC4626.sol";
+import "@ubk-labs/ubk-commons/contracts/mocks/MockAggregatorV3.sol";
+
+contract UBKCommonsMocks {}
