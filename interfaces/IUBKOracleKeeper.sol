06fc72bc6e990bd5cc6f3056773fefb325b2d15f
Nischinth Murari <murarins92@gmail.com>
2026-04-29 06:39:57 -0400

    Add SPDX and pragma to IUBKOracleKeeper

--- interfaces/IUBKOracleKeeper.sol
+++ interfaces/IUBKOracleKeeper.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
 interface IUBKOracleKeeper {
     event KeeperIntervalUpdated(uint256 oldInterval, uint256 newInterval);
     event KeeperTaskCompleted(
